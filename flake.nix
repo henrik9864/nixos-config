45a1b273258d65fef7d791ad49fe36b32c1588b4
Henrik Strocka <henstr@hotmail.com>
2026-04-04 22:48:47 +0200

    first working personal desktop

--- flake.nix
+++ flake.nix
@@ -3,10 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v1.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, vscode-server, ... }: {
+  outputs = inputs@{ self, nixpkgs, vscode-server, lanzaboote, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
@@ -30,6 +38,39 @@
       modules = [
         vscode-server.nixosModules.default
         ./hosts/docker/configuration.nix
+      ];
+    };
+
+    nixosConfigurations.nixos-personal = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      specialArgs = {
+        inherit inputs;
+      };
+
+      modules = [
+        ./hosts/personal-desktop/configuration.nix
+
+        lanzaboote.nixosModules.lanzaboote
+
+        ({ pkgs, lib, ... }: {
+
+          environment.systemPackages = [
+            # For debugging and troubleshooting Secure Boot.
+            pkgs.sbctl
+          ];
+
+          # Lanzaboote currently replaces the systemd-boot module.
+          # This setting is usually set to true in configuration.nix
+          # generated at installation time. So we force it to false
+          # for now.
+          boot.loader.systemd-boot.enable = lib.mkForce false;
+
+          boot.lanzaboote = {
+            enable = true;
+            pkiBundle = "/var/lib/sbctl";
+          };
+        })
       ];
     };
   };
